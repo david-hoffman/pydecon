67fdeffed39c683bd8c58f61b09aef099fb40ea5
David Hoffman <dave.p.hoffman@gmail.com>
2016-10-31 14:10:00 -0400

    Add psf3dclean

--- clean.py
+++ clean.py
@@ -68,3 +68,51 @@ def psf2dclean(psf, exp_kwargs, ns=4):
     # ifft
     cleaned_psf = np.real(fftshift(ifftn(ifftshift(otf))))
     return cleaned_psf
+
+
+def psf3dclean(psf, exp_kwargs, ns=4):
+    """A function to clean up a 3D PSF in both real and reciprocal space
+
+    Parameters
+    ----------
+    """
+
+    # break out kwargs
+    na, ni, wl, res, zres = (exp_kwargs["na"], exp_kwargs["ni"],
+                             exp_kwargs["wl"], exp_kwargs["res"],
+                             exp_kwargs["zres"])
+    # psf can be approximated by a gaussian with sigma = 0.45 * wl / (2 * na)
+    # https://en.wikipedia.org/wiki/Airy_disk
+    # filter in x-space by assuming that there is no data at ns sigma
+    cleaned_psf = psf.copy()
+    xtot, xres = _make_xspace(psf, (zres, res, res))
+    r = np.hypot(*xtot[1:])
+    z = xtot[0]
+    theta = np.arcsin(na / ni)
+    # make a double sided cone
+    mask = r - abs(z) * np.tan(theta) > ns * 0.45 * wl / (2 * na)
+    cleaned_psf[mask] = 0
+    # TODO: switch fft's to rfft's
+    otf = fftshift(fftn(ifftshift(cleaned_psf)))
+    # make the coordinate system, assume data is centered
+    ktot, kres = _make_kspace(cleaned_psf, (zres, res, res))
+    # we care about radius only
+    kr = np.hypot(*ktot[1:])
+    kz = ktot[0]
+    kr_max = ni / wl  # the radius of the spherical shell
+    kr_0 = na / wl  # the offset of the circle from the origin
+    # z displacement of circle's center
+    z0 = np.sqrt(kr_max ** 2 - kr_0 ** 2)
+    cent_kr = kr - kr_0
+    # calculate top half
+    # onehalf = np.hypot(cent_kr, kz - z0 - z_offset * dkz) <= kr_max
+    onehalf = np.hypot(cent_kr, kz - z0) > kr_max
+    # calculate bottom half
+    # otherhalf = np.hypot(cent_kr, kz + z0 - z_offset * dkz) <= kr_max
+    otherhalf = np.hypot(cent_kr, kz + z0) > kr_max
+    mask = np.logical_or(otherhalf, onehalf)
+    # mask = # more stuff here
+    otf[mask] = 0
+    # ifft
+    cleaned_psf = np.real(fftshift(ifftn(ifftshift(otf))))
+    return cleaned_psf
