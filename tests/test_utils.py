501b86dd408089210b651b486a960ae054617c81
David Hoffman <dave.p.hoffman@gmail.com>
2017-01-20 14:13:17 -0500

    Tests updated for previous commit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,59 +23,70 @@ def make_random_blob(ndims, size):
     return np.exp(-r2)
 
 
-class TestRadialAverage(unittest.TestCase):
+class TestRadialAverageFuncs(unittest.TestCase):
     """Radial average function tester class"""
 
     def setUp(self):
         """set up variables for testing"""
         self.test_data2d = make_random_blob(2, 128)
         self.test_data3d = make_random_blob(3, 128)
+        self.radavg2d = radialavg(self.test_data2d)
+        # radial average along z
+        self.radavg3d = radialavg(self.test_data3d)
 
     def test_returns2d(self):
         """make sure 2d data turns into 1d"""
-        radavg = radialavg(self.test_data2d)
-        assert_equals(radavg.ndim, 1)
+        assert_equals(self.radavg2d.ndim, 1)
 
     def test_returns3d(self):
         """make sure 3d data turns into 2d"""
-        radavg = radialavg(self.test_data3d)
-        assert_equals(radavg.ndim, 2)
+        assert_equals(self.radavg3d.ndim, 2)
 
-    def test_returns2d_odd(self):
+    def test_returns2d_even(self):
         """make sure 2d data turns into 1d that is odd in length"""
-        radavg = radialavg(self.test_data2d)
-        assert_true(radavg.size % 2)
+        assert_true(self.radavg2d.size % 2)
 
-    def test_returns3d_odd(self):
-        """make sure 3d data turns into 2d that is odd along the r direction."""
-        radavg = radialavg(self.test_data3d)
-        assert_true(radavg.shape[1] % 2)
+    def test_returns3d_even(self):
+        """make sure 3d data turns into 2d that is odd along the r
+        direction."""
+        assert_true(self.radavg3d.shape[1] % 2)
 
     def test_self_consistency2d(self):
         """Make sure that expanded radialavg makes sense"""
-        # radialavg
-        radavg = radialavg(self.test_data2d)
         # expand
-        expanded = expand_radialavg(radavg)
+        expanded = expand_radialavg(self.radavg2d)
         center = np.array(expanded.shape) // 2
         # test center lines
-        assert_allclose(expanded[center[0], center[1]:], radavg)
+        assert_allclose(expanded[center[0], center[1]:], self.radavg2d)
         assert_allclose(expanded[center[0], center[1]:],
                         expanded[center[0]:, center[1]])
 
     def test_self_consistency3d(self):
         """Make sure that expanded radialavg is radially averaged to the
         same thing, 3d"""
-        # radialavg
-        radavg = radialavg(self.test_data3d)
         # expand
-        expanded = expand_radialavg(radavg)
+        expanded = expand_radialavg(self.radavg3d)
         center = np.array(expanded.shape) // 2
         # test center lines
-        assert_allclose(expanded[:, center[1], center[2]:], radavg)
+        assert_allclose(expanded[:, center[1], center[2]:], self.radavg3d)
         assert_allclose(expanded[:, center[1], center[2]:],
                         expanded[:, center[1]:, center[2]])
 
+    def test_fft_2d(self):
+        """Test that fft is real"""
+        expanded = expand_radialavg(self.radavg2d)
+        fft_data = np.fft.ifftn(np.fft.fftshift(expanded))
+        assert_true(abs(fft_data.imag).max() < np.finfo(fft_data.dtype).eps)
+
+    def test_fft_3d(self):
+        """Test that fft is real"""
+        expanded = expand_radialavg(self.radavg3d)
+        # we don't know where the max value is along z so we only want
+        # to take the fft along the radially averaged direction.
+        fft_data = np.fft.ifftn(np.fft.fftshift(expanded), axes=(1, 2))
+        max_val = abs(fft_data.imag).max()
+        eps = np.finfo(fft_data.dtype).eps
+        assert_true(max_val < eps, "{} !< {}".format(max_val, eps))
 
 class TestEnsurepositive(unittest.TestCase):
     """test ensure positive for proper behaviour"""
