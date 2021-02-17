484f01633027f187dfe5005c176e0b966718905c
David Hoffman <dave.p.hoffman@gmail.com>
2021-02-17 12:25:31 -0800

    First stab at #4

---
+++ pydecon/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __init__.py
+"""
+A package to perform image/volume deconvolution with emphasis on microscopy
+data.
+
+Copyright (c) 2016, David Hoffman
+"""
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
